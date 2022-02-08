f4f394e3c145e0d3cddd322a891273942dfd6d1c
MuhammadAqib-coder <mshohw@gmail.com>
2022-02-08 16:12:48 +0500

    modification in searchDelegate

--- lib/util/data_search.dart
+++ lib/util/data_search.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_doc_sqflite/models/customer_hisab.dart';
-import 'package:flutter_doc_sqflite/widgets/show_list_detail.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 class DataSearch extends SearchDelegate<String> {
   var hisab = Hive.box<CustomerHisab>("hisab");
+  var list;
 
   @override
   List<Widget>? buildActions(BuildContext context) {
@@ -14,7 +14,12 @@ class DataSearch extends SearchDelegate<String> {
       IconButton(
         icon: Icon(Icons.clear),
         onPressed: () {
-          query = "";
+          if (query.isEmpty) {
+            close(context, "");
+          } else {
+            query = "";
+            showSuggestions(context);
+          }
         },
       )
     ];
@@ -37,13 +42,22 @@ class DataSearch extends SearchDelegate<String> {
   @override
   Widget buildResults(BuildContext context) {
     // TODO: implement buildResults
-    throw UnimplementedError();
+    return ListView.builder(
+      itemCount: list.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: Icon(Icons.book),
+          title: Text(list[index].name),
+          subtitle: Text("Rs ${list[index].price}   ${list[index].date}"),
+        );
+      },
+    );
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
     // TODO: implement buildSuggestions
-    var list = query.isEmpty
+    list = query.isEmpty
         ? hisab.values.toList()
         : hisab.values
             .where((value) => value.name.toLowerCase().contains(query))
@@ -59,13 +73,28 @@ class DataSearch extends SearchDelegate<String> {
             itemBuilder: (context, index) {
               return ListTile(
                 onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (_) => ListDetail(list: list)));
+                  showResults(context);
                 },
                 leading: Icon(Icons.book),
-                title: Text(list[index].name),
+                title: RichText(
+                  text: TextSpan(
+                    text: list[index].name.substring(0,query.length),
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0
+                    ),
+                    children: [
+                      TextSpan(
+                        text: list[index].name.substring(query.length),
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 20.0
+                        )
+                      )
+                    ]
+                  ),
+                ),
                 subtitle: Text(list[index].price.toString()),
               );
             },
